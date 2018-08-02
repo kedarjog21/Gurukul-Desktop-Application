e4cf4e7419ebd757738a8071fb217f2d8dca6946
kedarjog21 <37667652+kedarjog21@users.noreply.github.com>
2018-08-02 19:58:53 +0530

    HomePage
    This is GUI with close operation working ,No other buttons are working

---
+++ HomePage.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import javax.swing.*;
+
+public class HomePage extends JFrame
+  { 
+  	JFrame frame;
+
+  	HomePage()
+  	 { 
+  	 	setTitle(" GURUKUL ");  // Title for window
+
+  	    JButton button1 = new JButton(" Worksheet ");	//creating Button 1 
+        JButton button2 = new JButton(" Payment ");
+        JButton button3 = new JButton(" Add/Modify ");
+
+        button1.setBounds(350,135,215,55);
+        button2.setBounds(350,200,215,55);
+        button3.setBounds(350,265,215,55);
+
+        add(button1);
+        add(button2);
+        add(button3);
+        
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,400);
+
+
+        setLayout(null);
+        setVisible(true);
+
+
+  	   }
+        
+     public static void main(String args[])
+       {
+       	  new HomePage();
+       }
+
+    }
